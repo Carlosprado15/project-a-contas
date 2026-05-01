42aa3d2f80b82f2f77cf0417572be37d076a4d15
Carlosprado15 <kaduprado6518@gmail.com>
2026-05-01 13:54:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-// PROJECT A - Service Worker (apenas para cache e instalação PWA)
-const CACHE_NAME = 'project-a-v1';
-const urlsToCache = ['./', './index.html'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
